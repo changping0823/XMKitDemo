e275b801cd92d53376ea1da46dc90bfb190bc49c
changping0823 <993195264@qq.com>
2018-06-05 16:45:15 +0800

    1

---
+++ XMKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMKitDemo
+//
+//  Created by 任长平 on 2018/6/5.
+//  Copyright © 2018年 任长平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMKitDemo
+//
+//  Created by 任长平 on 2018/6/5.
+//  Copyright © 2018年 任长平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
